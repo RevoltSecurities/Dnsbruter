0c7489326b3972e6c4c2b76223dabca4b8ab39a3
RevoltSecurities <119435129+RevoltSecurities@users.noreply.github.com>
2024-12-17 18:07:55 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as streamr:
+    long_description = streamr.read()
 
-    
 setup(
     name='DNSBruter',
-    version='1.0.5',
-    author='D.Sanjai Kumar @RevoltSecurities',
+    version='1.0.6',
+    author='D. Sanjai Kumar @RevoltSecurities',
     author_email='bughunterz0047@gmail.com',
-    
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/RevoltSecurities/Dnsbruter",
-    description='dnsbruter is a powerfull tool for asynchronous dns brutforcing and fuzzing with wildcard detection',
+    description='dnsbruter is a powerful tool for asynchronous DNS bruteforcing and fuzzing with wildcard detection',
     packages=find_packages(),
     install_requires=[
         'aiodns>=3.2.0',
@@ -21,7 +23,7 @@ setup(
         'Requests>=2.32.3',
         'setuptools>=68.1.2',
         'uvloop>=0.21.0'
-        ],
+    ],
     entry_points={
         'console_scripts': [
             'dnsbruter = dnsbruter.dnsbruter:main'
